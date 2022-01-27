eb97d8ee13efb3738973775492252b7d11ba513b
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-01-27 10:56:38 -0300

    created db model  / add data

--- contacts/models.py
+++ contacts/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=255)
@@ -14,3 +17,6 @@ class Contact(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
