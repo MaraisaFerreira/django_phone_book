1a57f21232761e60545b9d931a987c8bfeea847d
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-01-27 10:56:56 -0300

    display data changed

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name',
+                    'phone_number', 'email', 'created_at', 'category')
+
+    list_display_links = ('first_name', 'last_name')
+    list_filter = ('category',)
+    list_per_page = 50
+
+    search_fields = ('first_name', 'last_name', 'phone_number', 'email')
+
+
+admin.site.register(Category)
+admin.site.register(Contact, ContactAdmin)
