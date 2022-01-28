d1bd0f55322f939aeeac78c16f269fcec52d628e
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-01-28 10:22:46 -0300

    edit fields (phone / is_visible) add in admin area

--- contacts/admin.py
+++ contacts/admin.py
@@ -4,13 +4,15 @@ from .models import Category, Contact
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name',
-                    'phone_number', 'email', 'created_at', 'category')
+                    'phone_number', 'email', 'created_at', 'category',
+                    'is_visible')
 
     list_display_links = ('first_name', 'last_name')
     list_filter = ('category',)
-    list_per_page = 50
+    list_per_page = 25
 
     search_fields = ('first_name', 'last_name', 'phone_number', 'email')
+    list_editable = ('phone_number', 'is_visible')
 
 
 admin.site.register(Category)
