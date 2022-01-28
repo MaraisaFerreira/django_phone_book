3975d5840447da2418c09e664c14a9c2fa2aa3af
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-01-28 11:58:20 -0300

    filter and order values to return

--- contacts/views.py
+++ contacts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contact
 from django.core.paginator import Paginator
+from django.http import Http404
 
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.order_by('first_name').filter(is_visible=True)
     paginator = Paginator(contacts, 20)
 
     page = request.GET.get('page')
@@ -16,6 +17,10 @@ def index(request):
 
 def single_contact(request, contact_id):
     contact = get_object_or_404(Contact, id=contact_id)
+
+    if not contact.is_visible:
+        raise Http404()
+
     return render(request, 'contacts/single_contact.html', {
         'contact': contact
     })
